9f6b89f9f24cf2c5259bbf262baf1c27310f83ce
Gabriel Nuñez <gabrielne310@gmail.com>
2026-05-30 21:16:01 -0300

    estructura inicial de prestamo

--- prestamos.py
+++ prestamos.py
@@ -1,27 +1,14 @@
+# MÓDULO: prestamos.py (Responsable: Gabriel)
+
+from datetime import date, timedelta
+
+#Impotar funcion de validacion 
+from validaciones import validar_numero
+
+#Importar listas Globales
+from peliculas import peliculas
+from socios import socios
+
+#Lista Global vacia de prestamos 
+
 prestamos = []
-
-
-def menu_prestamos():
-
-    while True:
-
-        print("\n=== PRÉSTAMOS ===")
-        print("1. Registrar préstamo")
-        print("2. Registrar devolución")
-        print("9. Volver")
-        
-
-        opcion = input("Seleccione una opción: ")
-
-        match opcion:
-
-            case "1":
-                print("Función pendiente")
-
-            case "2":
-                print("Función pendiente")
-
-            case "9":
-                break
-            case _:
-                print("Opción inválida")
